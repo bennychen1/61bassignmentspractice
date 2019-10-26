3d186124ef67ea6a5b1af9506fb44d58ad1cb8dd
Benny Chen <bechen1@berkeley.edu>
2019-10-26 11:18:55 -0700

    list test

--- hw2/IntList2/ListsTest.java
+++ hw2/IntList2/ListsTest.java
@@ -7,8 +7,21 @@ import static org.junit.Assert.*;
  */
 
 public class ListsTest {
-    /** FIXME
-     */
+
+    @Test
+    public void testNaturalRuns() {
+        IntList A = IntList.list(2, 3, 5, 1, 7, 9, 10, 9, 3);
+        IntList B = IntList.list(1, 1, 1);
+        IntList C = IntList.list(3);
+
+        IntList2 ARuns = IntList2.list(new int[][]{{2, 3, 5}, {1, 7, 9, 10}, {9},{3}});
+        IntList2 BRuns = IntList2.list(new int[][]{{1}, {1}, {1}});
+        IntList2 CRuns = IntList2.list(new int[][]{{3}});
+
+        assertEquals(ARuns, Lists.naturalRuns(A));
+        assertEquals(BRuns, Lists.naturalRuns(B));
+        assertEquals(CRuns, Lists.naturalRuns(C));
+    }
 
     // It might initially seem daunting to try to set up
     // Intlist2 expected.
