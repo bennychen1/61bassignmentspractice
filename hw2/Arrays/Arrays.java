afcbcfdcd2454f5361dbc8551538417d95b0eafe
Benny Chen <bechen1@berkeley.edu>
2019-10-26 13:39:05 -0700

    test arrays

--- hw2/Arrays/Arrays.java
+++ hw2/Arrays/Arrays.java
@@ -12,14 +12,47 @@ class Arrays {
      *  the elements of B. */
     static int[] catenate(int[] A, int[] B) {
         /* *Replace this body with the solution. */
-        return null;
+        if (A == B || B == null) {
+            return A;
+        }
+
+        if (A == null) {
+            return B;
+        }
+
+        int[] res = new int[A.length + B.length];
+
+        int p = 0;
+
+        for (int count = 0; count < A.length; count++, p++) { /* counters run everytime besides the first iteration */
+            res[count] = A[count];
+        }
+
+        for (int count = 0; count < B.length; count++, p++) {
+            res[p] = B[count];
+        }
+
+        return res;
     }
 
     /** Returns the array formed by removing LEN items from A,
      *  beginning with item #START. */
     static int[] remove(int[] A, int start, int len) {
         /* *Replace this body with the solution. */
-        return null;
+
+        if (A == null || A.length == 0) {
+            return A;
+        }
+
+        if (start + len > A.length) {
+            retu
+        }
+
+        int[] res = new int[A.length - len];
+
+        int p = 0;
+
+        return new int[4];
     }
 
     /* E. */
--- hw2/Arrays/ArraysTest.java
+++ hw2/Arrays/ArraysTest.java
@@ -6,9 +6,35 @@ import static org.junit.Assert.*;
  */
 
 public class ArraysTest {
-    /** FIXME
-     */
 
+    @Test
+    public void testCatenate() {
+        int[] A = new int[]{1, 2, 3};
+        int[] B = new int[]{4, 5, 6};
+        int[] BNull = new int[]{};
+        int[] CNull = new int[]{};
+        int[] AB = new int[]{1, 2, 3, 4, 5, 6};
+
+        assertArrayEquals(AB, Arrays.catenate(A, B));
+        assertArrayEquals(A, Arrays.catenate(A, BNull));
+        assertArrayEquals(B, Arrays.catenate(BNull, B));
+        assertArrayEquals(new int[]{}, Arrays.catenate(CNull, BNull));
+    }
+
+    @Test
+    public void testRemove() {
+        int[] A = new int[]{1, 2, 3, 4, 5, 6};
+        int[] B = new int[]{1, 2, 3};
+
+        int[] Ares = new int[]{1, 2, 3, 5};
+        int[] Bres = new int[]{1, 2};
+
+        assertArrayEquals(Ares, Arrays.remove(A, 3, 2));
+        assertArrayEquals(Bres, Arrays.remove(B, 2, 5));
+        assertArrayEquals(A, Arrays.remove(A, 1, 0));
+
+
+    }
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ArraysTest.class));
     }
