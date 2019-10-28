1fb34df7ca61199e85526b849d0c65aacd5843d2
Benny Chen <bechen1@berkeley.edu>
2019-10-28 14:53:36 -0700

    test for accumulate

--- hw2/ImageRescaler/MatrixUtilsTest.java
+++ hw2/ImageRescaler/MatrixUtilsTest.java
@@ -27,6 +27,24 @@ public class MatrixUtilsTest {
         assertArrayEquals(onesAm, MatrixUtils.accumulateVertical(ones)); /* Test equals */
     }
 
+    @Test
+    public void testAccumulate() {
+
+        double[][] e = new double[][]{{1, 5, 10}, {6, 7, 3}, {13, 9, 11}};
+        double[][] eCopy = new double[][]{{1, 5, 10}, {6, 7, 3}, {13, 9, 11}};
+        double[][] amV = new double[][]{{1, 5, 10}, {7, 8, 8}, {20, 16, 19}};
+        double[][]amO = new double[][]{{1, 6, 16}, {6, 8, 9}, {13, 15, 19}};
+
+        double[][] ones = new double[][]{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        double[][] onesAm = new double[][]{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+
+        assertArrayEquals(amV, MatrixUtils.accumulate(e, MatrixUtils.Orientation.VERTICAL));
+        assertArrayEquals(amO, MatrixUtils.accumulate(e, MatrixUtils.Orientation.HORIZONTAL));
+        assertArrayEquals(eCopy, e);
+
+        assertArrayEquals(onesAm, MatrixUtils.accumulate(ones, MatrixUtils.Orientation.HORIZONTAL));
+    }
+
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(MatrixUtilsTest.class));
     }
